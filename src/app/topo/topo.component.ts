698f49321055c76d4435ff94ae7e7735639d7631
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-06-18 20:36:21 -0300

    atualizando

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { Observable, Subject } from 'rxjs';
+import { switchMap } from "rxjs/operators";
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-topo',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Observable<Oferta[]>;
+  public subjectPesquisa: Subject<string> = new Subject<string>();
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+      .pipe(switchMap((termo: string) => {
+        return this.ofertasService.pesquisaOfertas()
+      }));
   }
 
+  public pesquisa(termoDaBusca: string): void {
+    this.subjectPesquisa.next()
+  }
 }
